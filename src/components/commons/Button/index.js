0ae0f90827330c3e38bf3e5799670d4879d29450
EduKaufmann <44790023+EduKaufmann@users.noreply.github.com>
2021-03-09 01:46:05 -0300

    aula 07

--- src/components/commons/Button/index.js
+++ src/components/commons/Button/index.js
@@ -1,6 +1,7 @@
 import styled, { css } from 'styled-components';
 import get from 'lodash/get';
 import { TextStyleVariantsMap } from '../../foundation/Text';
+import { breakpointsMedia } from '../../../theme/utils/breakpointsMedia';
 
 const ButtonGhost = css`
     color: ${(props) => get(props.theme, `colors.${props.variant}.color`)};
@@ -8,13 +9,8 @@ const ButtonGhost = css`
 `;
 
 const ButtonDefault = css`
-    color: white;
-    background-color: ${function (props) {
-        return get(props.theme, `colors.${props.variant}.color`)
-    }};
-    color: ${function (props) {
-        return get(props.theme, `colors.${props.variant}.contrastText`)
-    }};
+    color: ${({ theme, variant }) => get(theme, `colors.${variant}.contrastText`)};
+    background-color: ${({ theme, variant }) => get(theme, `colors.${variant}.color`)};
 `;
 
 export const Button = styled.button`
@@ -23,18 +19,20 @@ export const Button = styled.button`
     padding: 12px 26px;
     font-weight: bold;
     opacity: 1;
-    border-radius: 8px;
-
-    ${TextStyleVariantsMap.smallestException}
-
-    ${function (props) {
-        if (props.ghost) {
-            return ButtonGhost;
-        }
-        return ButtonDefault;
-    }}
     transition: opacity ${({ theme }) => theme.transition};
     border-radius: ${({ theme }) => theme.borderRadius};
+
+    ${breakpointsMedia({
+    xs: css`
+      ${TextStyleVariantsMap.smallestException}
+    `,
+    md: css`
+      padding: 12px 43px;
+      ${TextStyleVariantsMap.paragraph1}
+    `,
+})}
+
+    ${({ ghost }) => (ghost ? ButtonGhost : ButtonDefault)}
     &:hover,
     &:focus {
         opacity: .5;
